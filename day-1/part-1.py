57c1930282af0bfb28088742290dc81860c7c838
LoickBde <loick.baude@decathlon.com>
2022-12-01 12:38:44 +0100

    day 1 part 1 complete

--- day-1/part-1.py
+++ day-1/part-1.py
@@ -1,1 +1,20 @@
-print('hello aoc 2022')
+rawCalories = []
+totalCalories = 0
+maxCaloriesCarried = 0
+
+with open('input.txt') as f:
+    rawCalories =  [line.rstrip() for line in f]
+
+currentCaloriesCarried = 0
+for i in range(0, len(rawCalories)):
+    if rawCalories[i] == "":
+        if currentCaloriesCarried > maxCaloriesCarried:
+            maxCaloriesCarried = currentCaloriesCarried
+        currentCaloriesCarried = 0
+        continue
+    calories = int(rawCalories[i])
+    totalCalories += calories
+    currentCaloriesCarried += calories
+        
+print(f"Max calories carried : {maxCaloriesCarried}") 
+print(f"Total calories carried : {totalCalories}") 
